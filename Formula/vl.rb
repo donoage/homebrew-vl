b5d412b8a36017ef63f2cfd6c09ef3961d6368c5
Stephen Bae <donoage@gmail.com>
2025-05-08 22:20:38 -0400

    Fix dollar sign escaping in script

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -2,8 +2,8 @@ class Vl < Formula
   desc "Command-line tool to display stock volume leaders"
   homepage "https://github.com/donoage/homebrew-vl"
   url "https://raw.githubusercontent.com/donoage/homebrew-vl/v1.0.2/bin/vl"
-  sha256 "afb5c74ab993e2f8fb8190dcf76ba71c7e18fd84c5ee6080864d1f081114940b"
-  version "1.0.2"
+  sha256 "b9ae713c176e4fe388c11ec25998758bd9ab813498e920eda090f27840779666"
+  version "1.0.3"
 
   def install
     bin.install "vl"
