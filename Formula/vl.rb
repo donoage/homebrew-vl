17d3704f80d36139e9a452426b50486fb5076345
Stephen Bae <donoage@gmail.com>
2025-05-08 22:09:52 -0400

    Update formula to use v1.0.1 tag

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v1.0.0/bin/vl"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v1.0.1/bin/vl"
   sha256 "bb7026a769375a5110b11f1634ab8d18761bc8ab7b81c27c7d52cc8a4d19d6fd"
-  version "1.0.0"
+  version "1.0.1"
 
   def install
     bin.install "vl"
