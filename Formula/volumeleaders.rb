e38c3148896a8dd7a565f8a350d8b7bc28980a29
Stephen Bae <donoage@gmail.com>
2025-05-08 22:07:39 -0400

    Simplify formula to use raw GitHub URL

--- Formula/volumeleaders.rb
+++ Formula/volumeleaders.rb
@@ -1,16 +1,13 @@
 class Volumeleaders < Formula
   desc "Command-line tool to display stock volume leaders"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://github.com/donoage/homebrew-vl/releases/download/volumeleaders/volumeleaders"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v1.0.0/bin/volumeleaders"
   sha256 "bb7026a769375a5110b11f1634ab8d18761bc8ab7b81c27c7d52cc8a4d19d6fd"
   version "1.0.0"
-  bottle do
-    root_url "https://github.com/donoage/homebrew-vl/releases/download/volumeleaders"
-    sha256 cellar: :any_skip_relocation, monterey: "eeaa646cc346ccc5209960d2f950333f443b13cbe6629cdd62b99c6c35c52f1c"
-  end
 
   def install
     bin.install "volumeleaders"
+    system "chmod", "+x", "#{bin}/volumeleaders"
   end
 
   test do
