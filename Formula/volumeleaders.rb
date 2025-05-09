9eda7586d44d9f76839fe50fce13309e6ad3f533
Stephen Bae <donoage@gmail.com>
2025-05-08 22:03:35 -0400

    Add version to formula

--- Formula/volumeleaders.rb
+++ Formula/volumeleaders.rb
@@ -3,6 +3,7 @@ class Volumeleaders < Formula
   homepage "https://github.com/donoage/homebrew-vl"
   url "https://github.com/donoage/homebrew-vl/releases/download/volumeleaders/volumeleaders"
   sha256 "bb7026a769375a5110b11f1634ab8d18761bc8ab7b81c27c7d52cc8a4d19d6fd"
+  version "1.0.0"
   bottle do
     root_url "https://github.com/donoage/homebrew-vl/releases/download/volumeleaders"
     sha256 cellar: :any_skip_relocation, monterey: "eeaa646cc346ccc5209960d2f950333f443b13cbe6629cdd62b99c6c35c52f1c"
