a634dae5b66e381ce98e20c0dbcf582dd00a0206
Stephen Bae <donoage@gmail.com>
2025-05-13 00:19:55 -0400

    Simplify vl script to open two URLs without positioning or messages

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v1.0.7/bin/vl"
-  sha256 "13f44562b8cc9db9259e70c4723665dad20cd420ec07dd386d6a7c61e0bf26f9"
-  version "1.0.7"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v1.0.8/bin/vl"
+  sha256 "21946108badbcc4cd2f92f997bccf0577c0c97163a6bfcf02df321845836bf36"
+  version "1.0.8"
 
   def install
     bin.install "vl"
