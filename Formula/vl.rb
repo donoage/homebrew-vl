d838db3e2a19e809668ec951afd1f51b112e4dad
Stephen Bae <donoage@gmail.com>
2025-05-12 23:59:15 -0400

    Update vl script to open two URLs - 90 day default view and 30 day view with DarkPools and Sweeps enabled

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v1.0.5/bin/vl"
-  sha256 "32051ad2c953fbe5c33f33547109382342c7040f4f893f9f521d1daa9dcdb974"
-  version "1.0.5"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v1.0.7/bin/vl"
+  sha256 "13f44562b8cc9db9259e70c4723665dad20cd420ec07dd386d6a7c61e0bf26f9"
+  version "1.0.7"
 
   def install
     bin.install "vl"
