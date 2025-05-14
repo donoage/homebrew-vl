10f8719adf4cf992fb863b4ae3072b23cb100725
Stephen Bae <donoage@gmail.com>
2025-05-14 13:41:10 -0400

    Add automatic screen dimension detection for both macOS and Windows

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders (cross-platform)"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v2.0.0/bin/vl"
-  sha256 "1840a679fefb7b88bb0827001454839f143714eaa4fec581e71ad9f616d92c00"
-  version "2.0.0"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v2.1.0/bin/vl"
+  sha256 "655706f70cb760277754159d7afec60ebc684a5731f5d7894621e482796ae25d"
+  version "2.1.0"
   
   depends_on "python@3"
 
