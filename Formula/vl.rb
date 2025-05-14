352ea9e641fa03068218fdf2cc7351c8c1c1f5f0
Stephen Bae <donoage@gmail.com>
2025-05-14 14:00:48 -0400

    Add Parallels WSL support for browser opening

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders (cross-platform)"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v2.2.0/bin/vl"
-  sha256 "4bce5cc8542ceb56d19bc11f020f70dca10529d0d35fa46f0431a7a1a302306a"
-  version "2.2.0"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v2.3.0/bin/vl"
+  sha256 "1edf42f8ae970bac2bd6537c6bc26f56c8ad4542e46462f3526ecbd1e1df0022"
+  version "2.3.0"
   
   depends_on "python@3"
 
