ab49c8a8337c16fab916bb7bdce61d99f8d9da12
Stephen Bae <donoage@gmail.com>
2025-05-14 19:18:16 -0400

    Update formula to version 3.0.6

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders (cross-platform)"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.5/bin/vl"
-  sha256 "663a2396a54f81ee6974af972050addd5231f7677b65f3ae8a9183a2b494b992"
-  version "3.0.5"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.6/bin/vl"
+  sha256 "dbf60c0a8aac1f8c2e612aefbf71f5cf829b5e5d23d8b384ebdfdf590abfe306"
+  version "3.0.6"
   
   depends_on "python@3"
 
