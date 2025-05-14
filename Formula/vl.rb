90279431fff395fb6e24eb47953353a36670a687
Stephen Bae <donoage@gmail.com>
2025-05-14 19:39:25 -0400

    Fix URL refresh in existing browser windows (v3.0.10)

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders (cross-platform)"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.9/bin/vl"
-  sha256 "297cabb73b781e934a66080ab7ef21b60aa2fa0cecc926be2d866d63028545f2"
-  version "3.0.9"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.10/bin/vl"
+  sha256 "9185595f4d693cc5e1a7deca7ae3485ecb07f3597c68d39775111880925f1a34"
+  version "3.0.10"
   
   depends_on "python@3"
 
