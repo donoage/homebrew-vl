b42e1acee4ca1409a877ccb100494acfaffa4ca3
Stephen Bae <donoage@gmail.com>
2025-05-14 14:50:08 -0400

    Fix WSL UNC path issues when running Windows commands

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders (cross-platform)"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v2.4.0/bin/vl"
-  sha256 "db4dfe62437ea647c5b99da1b7defab93db827b9f98cd47d786aff86513fbb3b"
-  version "2.4.0"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v2.5.0/bin/vl"
+  sha256 "6cac37c15dfbda5e193196c8a06962a876df21a05fbdc673b0132217270286ec"
+  version "2.5.0"
   
   depends_on "python@3"
 
