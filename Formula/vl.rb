eec77154fa515c21928d7f33762f03b975f7b5c9
Stephen Bae <donoage@gmail.com>
2025-05-14 14:56:30 -0400

    Add Microsoft Edge fallback if Chrome is not found

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders (cross-platform)"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v2.5.0/bin/vl"
-  sha256 "6cac37c15dfbda5e193196c8a06962a876df21a05fbdc673b0132217270286ec"
-  version "2.5.0"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v2.6.0/bin/vl"
+  sha256 "44b37b195a1b4449580cdb53626f8a8187156a967fb148ea9057edb06515e5ee"
+  version "2.6.0"
   
   depends_on "python@3"
 
