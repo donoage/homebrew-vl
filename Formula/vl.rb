4269202a21919ce8d8b8eff9f12ee6c357cc7507
Stephen Bae <donoage@gmail.com>
2025-05-14 18:20:16 -0400

    Improved window positioning for Windows 11 using user32.dll API

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders (cross-platform)"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v2.8.0/bin/vl"
-  sha256 "7a156af024f6fac8b4dadd1719726476a5525df93ada59d6cd5538597d0848b8"
-  version "2.8.0"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v2.9.0/bin/vl"
+  sha256 "fc4e96d0842588a5e89f73a9e4b144adea4f8ca92af73afc77e07f082b11480c"
+  version "2.9.0"
   
   depends_on "python@3"
 
