bbe8803422225ba42fca75596d08025c471de952
Stephen Bae <donoage@gmail.com>
2025-05-14 19:29:03 -0400

    Fix WSL Parallels window detection for v3.0.8

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders (cross-platform)"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.7/bin/vl"
-  sha256 "3a2c21c5975f66753eb1d98c38d7312407831092f233a701fbf2a4b2bac073a4"
-  version "3.0.7"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.8/bin/vl"
+  sha256 "87928f97955c130731df68d7acc374708eec66ca855573cd87aad1ceda370b5f"
+  version "3.0.8"
   
   depends_on "python@3"
 
