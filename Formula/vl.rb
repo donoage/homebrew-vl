e4273f214567b98fbcacad6faba4d015f39afe2b
Stephen Bae <donoage@gmail.com>
2025-05-14 20:54:14 -0400

    Fix SHA256 hash in formula for v3.0.11

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -2,7 +2,7 @@ class Vl < Formula
   desc "Command-line tool to display stock volume leaders (cross-platform)"
   homepage "https://github.com/donoage/homebrew-vl"
   url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.11/bin/vl"
-  sha256 "6c2e6c1f683e096e2d3f186c6a78a24ee5216ee501724d6164d13c6cf6d52a87"
+  sha256 "6a65cf0793e1db350a1244272f0147a1696854f9db6f6ebbc12d73347e2f0908"
   version "3.0.11"
   
   depends_on "python@3"
