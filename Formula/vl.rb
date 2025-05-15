b2c8acdbcb837dbb36b617e00345cf76e8ad47db
Stephen Bae <donoage@gmail.com>
2025-05-14 21:10:53 -0400

    Update SHA256 hash for v3.0.13

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -2,7 +2,7 @@ class Vl < Formula
   desc "Command-line tool to display stock volume leaders (cross-platform)"
   homepage "https://github.com/donoage/homebrew-vl"
   url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.13/bin/vl"
-  sha256 "TO_BE_UPDATED"
+  sha256 "c8b57a5b223b27e1d0a08bc3d4092426bee003bc269d534f7080a65191aedcbe"
   version "3.0.13"
   
   depends_on "python@3"
