e2d2e48a3670354dfe62a36471927c452673aa64
Stephen Bae <donoage@gmail.com>
2025-05-14 20:52:35 -0400

    Update formula to version 3.0.7 with fixed window reuse

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders (cross-platform)"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.11/bin/vl"
-  sha256 "e087c5ba084ade6074fb8c8a24ad9cfa4574cdeaaa4bf41984167ae4c7879caa"
-  version "3.0.11"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.7/bin/vl"
+  sha256 "6c2e6c1f683e096e2d3f186c6a78a24ee5216ee501724d6164d13c6cf6d52a87"
+  version "3.0.7"
   
   depends_on "python@3"
 
