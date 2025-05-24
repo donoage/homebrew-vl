a98d20f3e7e585bff1a96f35165976517a2a66a1
Stephen Bae <donoage@gmail.com>
2025-05-23 23:42:02 -0400

    Update to v3.0.19: Increase TradeCount from 5 to 10

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders without closing extra browser windows"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.18/bin/vl"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.19/bin/vl"
   sha256 "c87c6caa282dc535075911d34b2c8dafedaa83fd0c9f4897cea0b2b3ac9c08f8"
-  version "3.0.18"
+  version "3.0.19"
   
   depends_on "python@3"
 
