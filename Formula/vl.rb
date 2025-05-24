4adb17ba965681bffb4661e2ca6fec4e64a4fe68
Stephen Bae <donoage@gmail.com>
2025-05-24 11:59:00 -0400

    Reduce MinDollars to 5M for DarkPools and Sweeps URLs

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders without closing extra browser windows"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.20/bin/vl"
-  sha256 "8e508fe6bbae995d8378e4fc92045f8f8d8b847e2909649b3985aae64bfdb103"
-  version "3.0.20"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.21/bin/vl"
+  sha256 "2d26df0079765309510bf9f1d2f61fc5a13a254b1acb762121bf82cd9840be5c"
+  version "3.0.21"
   
   depends_on "python@3"
 
