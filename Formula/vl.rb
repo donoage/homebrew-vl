d6030cc33c47ef9e07f4177b7826582f3eecd911
Stephen Bae <donoage@gmail.com>
2025-05-23 21:27:55 -0400

    Update to v3.0.18: Change URL endpoint to Chart0

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders without closing extra browser windows"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.17/bin/vl"
-  sha256 "eba614ed55f1c3ae51b7ceea9f43d260c90e7a0108a6b34ea74823fb6eb39adb"
-  version "3.0.17"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.18/bin/vl"
+  sha256 "001f657eb20507e453683a3973b3ba7a7df931c04a702afa803eb1bd3bb28541"
+  version "3.0.18"
   
   depends_on "python@3"
 
