2ed1bf7c9e7e234ece3409aa8b8e5093f6193c5f
Stephen Bae <donoage@gmail.com>
2025-05-23 23:42:33 -0400

    Fix SHA256 checksum for v3.0.19

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -2,7 +2,7 @@ class Vl < Formula
   desc "Command-line tool to display stock volume leaders without closing extra browser windows"
   homepage "https://github.com/donoage/homebrew-vl"
   url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.19/bin/vl"
-  sha256 "c87c6caa282dc535075911d34b2c8dafedaa83fd0c9f4897cea0b2b3ac9c08f8"
+  sha256 "dc9ea93913625dc5e401013a5406f80018c838cd9181425024031f8d4e5b7076"
   version "3.0.19"
   
   depends_on "python@3"
