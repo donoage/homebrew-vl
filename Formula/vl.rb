cb380071ebf0be723f2d268f29b2518e1068bad2
Stephen Bae <donoage@gmail.com>
2025-05-25 16:37:32 -0400

    VL Script v3.0.24: Keep Sweeps=1 while DarkPools=-1 for URL2 and URL4

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders without closing extra browser windows"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.23/bin/vl"
-  sha256 "08437e544e2632333afa6b1b7c9d0765f265949d3f99c8515f34a90712f05b7f"
-  version "3.0.23"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.24/bin/vl"
+  sha256 "aa58b4f969a17e8606d02e2158b32ef2bf1b2f9551de04464442cf665b414562"
+  version "3.0.24"
   
   depends_on "python@3"
 
