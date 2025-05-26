eeb018d5304276f8d4ba0fca5fdcbd9b6d18b4b9
Stephen Bae <donoage@gmail.com>
2025-05-26 14:37:26 -0400

    Update formula to version 3.0.29 with trading days calculation

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders and index charts"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.28/bin/vl"
-  sha256 "3f752a0bcc99aaee59af9a6d36b866283c2bbb240246abc7fc75a953bb15fccf"
-  version "3.0.28"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/v3.0.29/bin/vl"
+  sha256 "8aa67e662b43ae16cf021744a07acf3b89ad3ecefe1d10ecb652a3b112bcc596"
+  version "3.0.29"
   
   depends_on "python@3"
 
