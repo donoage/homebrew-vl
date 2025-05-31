790119e08e57d4ae607bca7444a49a966861f55f
Stephen Bae <donoage@gmail.com>
2025-05-30 23:13:48 -0400

    Update formula to v3.0.31 - TradeCount from 6 to 5

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders and index charts"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/84a5314e070b37e238497066a3a8f60cfc62d257/bin/vl"
-  sha256 "af640c224eacac9897f7cddb8d1cce2958387ea4096e7aa69cb224ce934a6da2"
-  version "3.0.30"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/6e82ff175383ac4745b077984b02c3e08f66afd8/bin/vl"
+  sha256 "a5b8ac6307e050bf81a7f60d552c78ca9dce5e8fe973078e997b0a48fcf3cbbc"
+  version "3.0.31"
   
   depends_on "python@3"
 
