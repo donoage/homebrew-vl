d4e998fcc4ef3fcedad0861ed75a9387eda1e0ed
Stephen Bae <donoage@gmail.com>
2025-06-21 16:00:33 -0400

    Update formulas with new commit hash and checksums - vl v3.0.36, vl-trades v1.0.1

--- Formula/vl-trades.rb
+++ Formula/vl-trades.rb
@@ -1,9 +1,9 @@
 class VlTrades < Formula
   desc "Command-line tool to display VolumeLeaders trade analysis URLs in positioned browser windows"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/5796beefb0dd9bf8ee06896638091499fcbf72a3/bin/vl-trades"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/13caacf6fa886f82167eef662155cc8cd9c38cb8/bin/vl-trades"
   sha256 "c8481d80715c018b2bee85b268332ad6d4d3327f612c7d406e475c8b05fea401"
-  version "1.0.0"
+  version "1.0.1"
   
   depends_on "python@3"
 
--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders and index charts"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/b67bf95620f8ed093da1a02064450c04760bcadd/bin/vl"
-  sha256 "e96fdfeb6272e2a91fbcc14e0febad625a135ff13863a23f8dc1f13a397885ec"
-  version "3.0.35"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/13caacf6fa886f82167eef662155cc8cd9c38cb8/bin/vl"
+  sha256 "6aa1cd347791aa2453c383f1ce383557de3ce1489de74031bb46040def2ece8f"
+  version "3.0.36"
   
   depends_on "python@3"
 
