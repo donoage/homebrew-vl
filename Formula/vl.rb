ddba0cb6508120f80613797faf3626b7e14d8297
Stephen Bae <donoage@gmail.com>
2025-09-13 21:57:45 -0400

    Update vl to version 3.0.37: Prevent closing of excess browser windows

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -2,8 +2,8 @@ class Vl < Formula
   desc "Command-line tool to display stock volume leaders and index charts"
   homepage "https://github.com/donoage/homebrew-vl"
   url "https://raw.githubusercontent.com/donoage/homebrew-vl/13caacf6fa886f82167eef662155cc8cd9c38cb8/bin/vl"
-  sha256 "6aa1cd347791aa2453c383f1ce383557de3ce1489de74031bb46040def2ece8f"
-  version "3.0.36"
+  sha256 "dfcbc9a2d21c32ef7456a9b2b189dc8ac396692a7cf085429116e1aa62df3e0e"
+  version "3.0.37"
   
   depends_on "python@3"
 
