09086c36b422ffe5f6faf743f3c10180a2882ea5
Stephen Bae <donoage@gmail.com>
2025-09-13 22:22:44 -0400

    Update vl to version 3.0.38: Change browser from Chrome to Firefox

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -2,8 +2,8 @@ class Vl < Formula
   desc "Command-line tool to display stock volume leaders and index charts"
   homepage "https://github.com/donoage/homebrew-vl"
   url "https://raw.githubusercontent.com/donoage/homebrew-vl/13caacf6fa886f82167eef662155cc8cd9c38cb8/bin/vl"
-  sha256 "dfcbc9a2d21c32ef7456a9b2b189dc8ac396692a7cf085429116e1aa62df3e0e"
-  version "3.0.37"
+  sha256 "1033504f468f96d947989ab2ef4ffaa0674131067cb5543db73c1f670ae2387d"
+  version "3.0.38"
   
   depends_on "python@3"
 
