a37b5184bdf74ce8aa4ec22b8e83f4b7f1f94f6d
Stephen Bae <donoage@gmail.com>
2025-09-13 23:26:59 -0400

    Update vl to version 3.0.39: Change URL ranges and set TradeCount to 5

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -2,8 +2,8 @@ class Vl < Formula
   desc "Command-line tool to display stock volume leaders and index charts"
   homepage "https://github.com/donoage/homebrew-vl"
   url "https://raw.githubusercontent.com/donoage/homebrew-vl/5ce11844ec8b2bcd7e64df6c7eb27bb76d8251d5/bin/vl"
-  sha256 "dfcbc9a2d21c32ef7456a9b2b189dc8ac396692a7cf085429116e1aa62df3e0e"
-  version "3.0.37"
+  sha256 "e961eab5fd555f9938b8443d5f47a1888252de742581cb339bfd1000942656ac"
+  version "3.0.39"
   
   depends_on "python@3"
 
