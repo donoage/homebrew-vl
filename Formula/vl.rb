99144ac500923437789bb09ccefd9aa73b0e4f25
Stephen Bae <donoage@gmail.com>
2025-09-22 21:28:10 -0400

    Update vl formula URL to point to latest commit

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,7 +1,7 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders and index charts"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/a37b5184bdf74ce8aa4ec22b8e83f4b7f1f94f6d/bin/vl"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/be40897e413731e56d76d7a8cf65e70685aaa95c/bin/vl"
   sha256 "821ac9016f316b4c98c64a9537a4e10fe736903b0c20fe4e90b0ec896fc3e736"
   version "3.0.41"
   
