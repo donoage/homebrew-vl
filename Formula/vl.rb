4df5c7b29aa8701b29179a8d4731f3febef73014
Stephen Bae <donoage@gmail.com>
2025-10-04 18:49:45 -0400

    Update vl formula to version 3.0.42 with DarkPools=1 and TradeCount updates

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders and index charts"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/be40897e413731e56d76d7a8cf65e70685aaa95c/bin/vl"
-  sha256 "821ac9016f316b4c98c64a9537a4e10fe736903b0c20fe4e90b0ec896fc3e736"
-  version "3.0.41"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/b206f57e0f5ac1febfb5578272b15efedd84aea5/bin/vl"
+  sha256 "aa1dbd01913fd11f794701dccbf4cd48d12947e3c4fd050ac972cebc0837181d"
+  version "3.0.42"
   
   depends_on "python@3"
 
