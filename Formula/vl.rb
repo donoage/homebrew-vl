339fd52a192bed94ab23a108ac52841aa4c92950
Stephen Bae <donoage@gmail.com>
2025-10-04 18:53:51 -0400

    Update vl formula to version 3.0.43 with TradeCount 20, 10, 10, 5

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders and index charts"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/b206f57e0f5ac1febfb5578272b15efedd84aea5/bin/vl"
-  sha256 "aa1dbd01913fd11f794701dccbf4cd48d12947e3c4fd050ac972cebc0837181d"
-  version "3.0.42"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/5a24b5acd7449ac2dc54bf9da580dd7f89a7b03f/bin/vl"
+  sha256 "9f1f4791575ff359b59e8653bc0265af74999d7954ec55d8d9fd5d28e5627596"
+  version "3.0.43"
   
   depends_on "python@3"
 
