90db50ac6c2e71055f026d7d93531107e0009deb
Stephen Bae <donoage@gmail.com>
2025-10-25 14:28:35 -0400

    Update vl formula to version 3.0.45 with window positioning fix

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders and index charts"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/ac2b507074514ad3f06ab7be124a697f68290910/bin/vl"
-  sha256 "2e7f32e01c9778da9825c72e3345217d9785ec24383b96ac57e4c47456d963c5"
-  version "3.0.44"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/18e4adb21e7d8062c2796ee0cc287fd72e304765/bin/vl"
+  sha256 "5cff1b449c777105099a4abe3c3a5cceb48c4e28f134d1378d36d89b818b673b"
+  version "3.0.45"
   
   depends_on "python@3"
 
