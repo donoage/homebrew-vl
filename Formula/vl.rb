a95c05e36e289ce89aa6beb8e54fc9a0d7e545eb
Stephen Bae <donoage@gmail.com>
2025-10-25 14:37:42 -0400

    Update vl formula to version 3.0.47 with window ID caching

--- Formula/vl.rb
+++ Formula/vl.rb
@@ -1,9 +1,9 @@
 class Vl < Formula
   desc "Command-line tool to display stock volume leaders and index charts"
   homepage "https://github.com/donoage/homebrew-vl"
-  url "https://raw.githubusercontent.com/donoage/homebrew-vl/18e4adb21e7d8062c2796ee0cc287fd72e304765/bin/vl"
-  sha256 "5cff1b449c777105099a4abe3c3a5cceb48c4e28f134d1378d36d89b818b673b"
-  version "3.0.45"
+  url "https://raw.githubusercontent.com/donoage/homebrew-vl/818b9b594a3a9513783622306e25a21d3f15def5/bin/vl"
+  sha256 "661a3537c2e7cc882401cf3c45e2cdd198ce5ff63a746a06be5ac998ea2157d9"
+  version "3.0.47"
   
   depends_on "python@3"
 
